34568923321a1e9efb4d39f9d29ee8db719f5fa9
Luís Felipe Ramos Ferreira <lframos_ferreira@outlook.com>
2025-02-14 12:20:30 -0300

    starting to add errors

---
+++ sofia/src/parser/error.rs
@@ -0,0 +1,7 @@
+use thiserror::Error;
+
+#[derive(Debug, Clone)]
+pub enum LexerError {}
+
+#[derive(Debug, Clone)]
+pub enum ParserError {}
--- sofia/src/parser/mod.rs
+++ sofia/src/parser/mod.rs
@@ -1,5 +1,6 @@
+mod error;
 pub mod lexer;
-pub mod token;
 pub mod parser;
+pub mod token;
 
 mod tests;

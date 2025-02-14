06a06ea8c4ce085863ca5a28fe4da881ff5d7a41
Luís Felipe Ramos Ferreira <lframos_ferreira@outlook.com>
2025-02-14 14:08:59 -0300

    adding some lexer errors

--- sofia/src/parser/error.rs
+++ sofia/src/parser/error.rs
@@ -1,7 +1,15 @@
 use thiserror::Error;
 
-#[derive(Debug, Clone)]
-pub enum LexerError {}
+#[derive(Debug, Error)]
+pub enum LexerError {
+    /// The lexer encountered a ! symbol without a = symbol right after it
+    #[error("! symbol does not contain a = right after it")]
+    NotEqualError,
+
+    /// The lexer encountered unmatched quotes
+    #[error("Unmatched quotes defining a string")]
+    UnmatchedQuotesError,
+}
 
 #[derive(Debug, Clone)]
 pub enum ParserError {}
